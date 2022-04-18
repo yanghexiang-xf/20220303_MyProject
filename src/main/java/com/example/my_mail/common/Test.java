66b122280e5a16ee9c580a9e10b58c14da0dadba
yhx <13721320340@163.com>
2022-04-18 20:46:33 +0800

    0314master

--- src/main/java/com/example/my_mail/common/Test.java
+++ src/main/java/com/example/my_mail/common/Test.java
@@ -12,5 +12,7 @@ public class Test {
     public Test() {
         System.out.println("--");
         System.out.println(11);
+        System.out.println(22);
+        System.out.println(33);
     }
 }
